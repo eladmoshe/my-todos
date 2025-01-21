a41202f73f0c2801321e4bb56d15eed7ddc9af88
Elad Moshe <elad.moshe@lemonade.com>
2025-01-21 14:45:41 +0200

    Simplify App component layout by removing unnecessary main wrapper

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -4,9 +4,7 @@ import TodoApp from "./components/TodoApp";
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50">
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <TodoApp />
-      </main>
+      <TodoApp />
     </div>
   );
 };
